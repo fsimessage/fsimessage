0a094740db74deea7d4150350aae298782055ba3
direct1986 <07ky@sina.com>
2023-07-06 15:31:44 +0800

    Update runall.py

--- runall.py
+++ runall.py
@@ -1,20 +1,56 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import os
 import subprocess
+import time
+from threading import Thread
+
+def run_python_file(file_path):
+    python_path = '/usr/bin/python3.11'  # 替换为实际的Python 3.11可执行文件路径
+    process = subprocess.Popen([python_path, file_path])
+    
+    # 等待子进程结束，设置超时时间为1分钟
+    process.communicate(timeout=60)
 
 def run_all_python_files():
     current_dir = os.getcwd()
     current_file = os.path.basename(__file__)
     files = [file_name for file_name in os.listdir(current_dir) if file_name.endswith('.py') and file_name != current_file]
 
-    python_path = '/usr/bin/python3.11'  # 替换为实际的Python 3.11可执行文件路径
+    threads = []
 
     for file_name in files:
-        # 启动子进程执行Python文件，指定使用Python 3.11版本
-        process = subprocess.Popen([python_path, file_name])
+        file_path = os.path.join(current_dir, file_name)
+        thread = Thread(target=run_python_file, args=(file_path,))
+        thread.start()
+        threads.append(thread)
 
-run_all_python_files()
+    # 等待所有线程执行完毕
+    for thread in threads:
+        thread.join()
+
+# 每分钟执行一次循环
+while True:
+    run_all_python_files()
+    time.sleep(60)
+
+
+# import os
+# import subprocess
+
+# def run_all_python_files():
+#     current_dir = os.getcwd()
+#     current_file = os.path.basename(__file__)
+#     files = [file_name for file_name in os.listdir(current_dir) if file_name.endswith('.py') and file_name != current_file]
+
+#     python_path = '/usr/bin/python3.11'  # 替换为实际的Python 3.11可执行文件路径
+
+#     for file_name in files:
+#         # 启动子进程执行Python文件，指定使用Python 3.11版本
+#         process = subprocess.Popen([python_path, file_name])
+
+# run_all_python_files()
 
 
 
